1040d9e0afc1a03a9870c086e5bf54bca09df19c
haze458 <37885088+haze458@users.noreply.github.com>
2019-04-24 14:13:24 +0100

    uploaded teat code in bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+
+var Discord = require('discord.io'); var logger = require('winston'); var auth = require('./auth.json'); 
+// Configure logger settings
+
+
+logger.remove(logger.transports.Console); logger.add(new logger.transports.Console, { colorize: true });
+
+
+
+
+bot.on('message', function (user, userID, channelID, message, evt) { 
+
+
+
+
+logger.level = 'debug'; 
+// Initialize Discord Bot 
+var bot = new Discord.Client({ token: auth.token, autorun: true });
+
+// Our bot needs to know if it will execute a command 
+
+// It will listen for messages that will start with `!`
+
+bot.on('ready', function (evt) { logger.info('Connected'); logger.info('Logged in as: '); logger.info(bot.username + ' - (' + bot.id + ')'); });
+
+
+ if (message.substring(0, 1) == '!') { var args = message.substring(1).split(' '); var cmd = args[0]; args = args.splice(1); switch(cmd) { 
+ // !ping 
+ case 'ping': bot.sendMessage({ to: channelID, message: 'Pong!' }); break; // Just add any case commands if you want to..
+  } } });
